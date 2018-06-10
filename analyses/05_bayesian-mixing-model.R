e7a692e221a7cc8f5705040f58af09204623b56a
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-06-10 15:11:52 +0200

    Update 05_bayesian-mixing-model.R

--- analyses/05_bayesian-mixing-model.R
+++ analyses/05_bayesian-mixing-model.R
@@ -7,7 +7,8 @@ source(here::here("Analyses-v2/04_amnt-weighted-iso-and-p_v2.R"))
 
 baseflow_means <- tibble(
     d18O = d18O_baseflow,
-    d2H = d2H_baseflow)
+    d2H = d2H_baseflow
+)
 baseflow_sds <- LRD_tidy %>%
     filter(source == "River") %>%
     select(d18O, d2H) %>%
@@ -32,7 +33,8 @@ simmr_in <- simmr_load(
         as.matrix(),
     source_names = c("baseflow", "rainfall"),
     source_means = rbind(baseflow_means, rainfall_means),
-    source_sds = rbind(baseflow_sds, rainfall_sds))
+    source_sds = rbind(baseflow_sds, rainfall_sds)
+)
 
 # Visualise
 plot(simmr_in)
