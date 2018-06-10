87cd98e383f0ad79c50b7a9e7bd126bcc804e63c
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-06-10 15:08:36 +0200

    Reindent setup.R

--- setup.R
+++ setup.R
@@ -7,12 +7,6 @@ if (!require(pacman)) install.packages("pacman")
 library(pacman)
 p_load(tidyverse, here, magrittr, readxl, stringr, lubridate, cowplot)
 
-# Get citations for all packages -----------------------------------------------
-
-p_load(bibtex)
-my_pkgs <- c("bibtex", "pacman", tidyverse_packages(), "here", "cowplot")
-bibtex::write.bib(entry = my_pkgs, file = "pkgs.bib")
-
 # Import data ------------------------------------------------------------------
 
 LRD_tidy <- read_csv(here::here("liesbeeck-data-tidy.csv"))[, -1]
@@ -21,6 +15,5 @@ colnames(LRD_tidy)[4] <- "source"
 harris_uct <- read_csv(here::here("harris-uct.csv"))
 
 extra_sites <- read_csv(here::here("johndaybldg-skeletongorge.csv")) %>%
-    mutate(
-        date_time_start = ymd_hm(date_time_start),
-        date_time_end   = ymd_hm(date_time_end))
+    mutate(date_time_start = ymd_hm(date_time_start),
+           date_time_end = ymd_hm(date_time_end))
