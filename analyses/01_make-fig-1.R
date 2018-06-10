64dca62e5c079ea6544745c43dde6de58264e782
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-06-10 15:09:38 +0200

    Update 01_make-fig-1.R

--- analyses/01_make-fig-1.R
+++ analyses/01_make-fig-1.R
@@ -1,7 +1,7 @@
 # Fig. 1. Storm timeline
 # R. van Mazijk, L.K. Smyth
 
-source(here::here("00_setup.R"))
+source(here::here("Analyses-v2/00_setup.R"))
 
 # Tidy data further for this figure --------------------------------------------
 
@@ -9,23 +9,21 @@ LRD_tidy_fig_1 <- LRD_tidy %>%
     # Make a new `type` column, replacing the old, that = "flow" when
     # source = river & type = amnt (aot d18O or d2H).
     gather("type", "amnt", c(d18O, d2H, amnt)) %>%
-    mutate(
-        type = as.factor(replace(
-            type,
-            (source == "River" & type == "amnt"),
-            "flow"))) %>%
-    mutate(
-        source_type = as.factor(paste0(
-            source, "_", type)))
+    mutate(type = as.factor(replace(type,
+                                    (source == "River" & type == "amnt"),
+                                    "flow"))) %>%
+    mutate(source_type = as.factor(paste0(source, "_", type)))
 
 # Reorder some factors:
 LRD_tidy_fig_1$source_type %<>% factor(c(
     "Rain_amnt", "River_flow",
     "Rain_d18O", "River_d18O",
-    "Rain_d2H",  "River_d2H"))
+    "Rain_d2H",  "River_d2H"
+))
 LRD_tidy_fig_1$type %<>% factor(c(
     "amnt", "flow",
-    "d18O", "d2H"))
+    "d18O", "d2H"
+))
 LRD_tidy_fig_1$amnt %<>% as.numeric()
 
 # Get time information when no rain
@@ -43,7 +41,8 @@ no_rain <- data.frame(
     date_time   = anti_join(river_times, rain_times),
     type        = rep("amnt",      10),
     amnt        = rep(0,           10),
-    source_type = rep("Rain_amnt", 10))
+    source_type = rep("Rain_amnt", 10)
+)
 LRD_tidy_fig_1 %<>% rbind(no_rain)
 
 # Calculate cumulative rainfall ------------------------------------------------
@@ -53,18 +52,13 @@ LRD_cumul_rain <- LRD_tidy_fig_1 %>%
     filter(source_type == "Rain_amnt") %>%
     mutate(cum_amnt = cumsum(amnt))
 
-insert_cumul_rain_type <- function(x)
-{
+insert_cumul_rain_type <- function(x) {
     x$type %<>% as.character()
-    for (i in 1:nrow(x))
-    {
+    for (i in 1:nrow(x)) {
         x$type[i] <-
-            if (x$source_type[i] == "cum_rain")
-            {
+            if (x$source_type[i] == "cum_rain") {
                 "cum_rain"
-            }
-            else
-            {
+            } else {
                 x$type[i]
             }
     }
@@ -73,13 +67,12 @@ insert_cumul_rain_type <- function(x)
 }
 
 LRD_tidy_gather_with_cumul_rain <- LRD_cumul_rain %>%
-    transmute(
-        code        = code,
-        source      = source,
-        date_time   = date_time,
-        type        = type,
-        amnt        = cum_amnt,
-        source_type = rep("cum_rain", times = nrow(LRD_cumul_rain))) %>%
+    transmute(code        = code,
+              source      = source,
+              date_time   = date_time,
+              type        = type,
+              amnt        = cum_amnt,
+              source_type = rep("cum_rain", times = nrow(LRD_cumul_rain))) %>%
     rbind(LRD_tidy_fig_1) %>%
     insert_cumul_rain_type()
 
@@ -89,7 +82,8 @@ LRD_tidy_gather_with_cumul_rain$type %<>% factor(levels = c(
     "amnt",
     "flow",
     "d18O",
-    "d2H"))
+    "d2H"
+))
 
 # Create the timeline plot -----------------------------------------------------
 
@@ -102,7 +96,8 @@ my_palette <- c(
     "#8dcf6d",  # light green
     "#983b33",  # dark red
     "#44782b",  # dark green
-    "#55a0c7")  # light blue
+    "#000000"   # black
+)
 
 # Create panel labels
 my_facet_labels <- c(
@@ -110,7 +105,8 @@ my_facet_labels <- c(
     amnt     = "B)",
     flow     = "C)",
     d18O     = "D)",
-    d2H      = "E)")
+    d2H      = "E)"
+)
 
 # Plot!
 base_plot <- ggplot(LRD_tidy_gather_with_cumul_rain) +
@@ -123,49 +119,45 @@ base_plot <- ggplot(LRD_tidy_gather_with_cumul_rain) +
 styled_plot <- base_plot +
     # Plot the different types (cum_rain, rain, river, d18O, d2H) as panels:
     # (with my custom facet labels!)
-    facet_grid(
-        scales = "free",
-        labeller = as_labeller(my_facet_labels),
-        type ~ .) +
+    facet_grid(scales = "free",
+               labeller = as_labeller(my_facet_labels),
+               type ~ .) +
     # Apply my colour palette, and rename the legend labels:
     guides(col = guide_legend(ncol = 1, label.position = "right")) +
     scale_color_manual(
         values = my_palette,
-        labels = c(
-            expression(paste("Cumulative rainfall (", {m}^{3}, ")")),
-            expression(paste("Rainfall (", {m}^{3}, {h}^{-1}, ")")),
-            expression(paste("Streamflow (", {m}^{3}, {h}^{-1}, ")")),
-            expression(paste(delta^{18}, "O rain (‰)")),
-            expression(paste(delta^{18}, "O stream (‰)")),
-            expression(paste(delta^{2}, "H rain (‰)")),
-            expression(paste(delta^{2}, "H stream (‰)"))),
+        labels = c(expression(paste("Cumulative rainfall (", {m}^{3}, ")")),
+                   expression(paste("Rainfall (", {m}^{3}, {h}^{-1}, ")")),
+                   expression(paste("Streamflow (", {m}^{3}, {h}^{-1}, ")")),
+                   expression(paste(delta^{18}, "O rain (‰)")),
+                   expression(paste(delta^{18}, "O stream (‰)")),
+                   expression(paste(delta^{2}, "H rain (‰)")),
+                   expression(paste(delta^{2}, "H stream (‰)"))),
         breaks = LRD_tidy_gather_with_cumul_rain %>%
             `$`(source_type) %>%
-            factor(levels = c(
-                "cum_rain",
-                "Rain_amnt",
-                "River_flow",
-                "Rain_d18O",
-                "River_d18O",
-                "Rain_d2H",
-                "River_d2H")) %>%
+            factor(levels = c("cum_rain",
+                              "Rain_amnt",
+                              "River_flow",
+                              "Rain_d18O",
+                              "River_d18O",
+                              "Rain_d2H",
+                              "River_d2H")) %>%
             levels())
 
 themed_plot <- styled_plot +
     theme_bw() +
-    theme(
-        legend.position    = "left",
-        legend.title       = element_blank(),
-        strip.background   = element_rect(fill = NA, colour = NA),
-        strip.text.y       = element_text(angle = 0),
-        panel.grid         = element_blank(),
-        panel.grid.major.x = element_line(colour = "grey50", linetype = "dashed"),
-        panel.grid.minor.x = element_line(colour = "grey75", linetype = "dashed"),
-        axis.text.x        = element_text(angle = 0),
-        axis.title.y       = element_blank())
+    theme(legend.position    = "left",
+          legend.title       = element_blank(),
+          strip.background   = element_rect(fill = NA, colour = NA),
+          strip.text.y       = element_text(angle = 0),
+          panel.grid         = element_blank(),
+          panel.grid.major.x = element_line(colour = "grey50", linetype = "dashed"),
+          panel.grid.minor.x = element_line(colour = "grey75", linetype = "dashed"),
+          axis.text.x        = element_text(angle = 0),
+          axis.title.y       = element_blank())
 
 fig_1 <- themed_plot
 
-tiff("fig-1.tiff", width = 25, height = 15, units = "cm", res = 300)
+tiff("fig-1.tiff", width = 25, height = 15, units = "cm", res = 500)
 fig_1
 dev.off()
