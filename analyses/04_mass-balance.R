3c00a68065788d904dbea56dc2a42b627508511f
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-06-26 20:05:55 +0200

    Write and document mass balance model w/ uncertainty propagation. Awaiting Adam's info Re: precision vs accuracy

--- analyses/04_mass-balance.R
+++ analyses/04_mass-balance.R
@@ -5,13 +5,15 @@ source(here::here("setup.R"))
 
 # Define baseflow isotope values -----------------------------------------------
 
-d18O_baseflow <- LRD_tidy %>%
+baseflow <- list(d18O = NULL, d2H = NULL)
+
+baseflow$d18O <- LRD_tidy %>%
     filter(source == "River") %>%
     slice(1:3) %$% {
         sum(d18O[1:3] * amnt[1:3]) %>%
             divide_by(sum(amnt[1:3]))
     }
-d2H_baseflow <- LRD_tidy %>%
+baseflow$d2H <- LRD_tidy %>%
     filter(source == "River") %>%
     slice(1:3) %$% {
         sum(d2H[1:3] * amnt[1:3]) %>%
@@ -20,159 +22,60 @@ d2H_baseflow <- LRD_tidy %>%
 
 # Separate rain and stream data ------------------------------------------------
 
-LRD_stream <- LRD_tidy %>%
+streamflow <- LRD_tidy %>%
     filter(source == "River") %>%
-    slice(-c(1:3))
-LRD_rain <- LRD_tidy %>%
+    slice(-c(1:3)) %>%
+    mutate(d18O_x_amnt = d18O * amnt,
+           d2H_x_amnt = d2H * amnt) %>%
+    summarise(weighted_d18O = sum(d18O_x_amnt) / sum(amnt),
+              weighted_d2H = sum(d2H_x_amnt) / sum(amnt)) %$%
+    list(d18O = weighted_d18O,
+         d2H = weighted_d2H)
+rain <- LRD_tidy %>%
     filter(source == "Rain") %>%
-    right_join(select(LRD_stream, date_time)) %>%
-    replace_na(list(code = "RAIN no code",
-                    d18O = 0,
-                    d2H = 0,
-                    source = "Rain",
-                    amnt = 0))
-
-# Initialise empty columns in data-frame ---------------------------------------
-
-LRD_stream$d18O_stream_t <- NA
-LRD_stream$d2H_stream_t <- NA
-LRD_stream$d18O_rain_t <- NA
-LRD_stream$d2H_rain_t <- NA
-LRD_stream$d18O_p_t <- NA
-LRD_stream$d2H_p_t <- NA
-
-# Run loop ---------------------------------------------------------------------
-
-for (t in 1:nrow(LRD_stream)) {
-    dE_stream_t <- dE_cum_amnt_weighted(LRD_stream[1:t, ])
-    dE_rain_t <- dE_cum_amnt_weighted(LRD_rain[1:t, ])
-    p_d18O_t <- p_iso(
-        dE_stream_t$weighted_d18O,
-        d18O_baseflow,
-        dE_rain_t$weighted_d18O
-    )
-    p_d2H_t <- p_iso(
-        dE_stream_t$weighted_d2H,
-        d2H_baseflow,
-        dE_rain_t$weighted_d2H
-    )
-    LRD_stream$d18O_stream_t[t] <- dE_stream_t$weighted_d18O
-    LRD_stream$d2H_stream_t[t] <- dE_stream_t$weighted_d2H
-    LRD_stream$d18O_rain_t[t] <- dE_rain_t$weighted_d18O
-    LRD_stream$d2H_rain_t[t] <- dE_rain_t$weighted_d2H
-    LRD_stream$d18O_p_t[t] <- p_d18O_t
-    LRD_stream$d2H_p_t[t] <- p_d2H_t
-}
-LRD_new <- LRD_stream
-
-# Look at results --------------------------------------------------------------
-
-# Look at final cumulative numbers:
-select(LRD_new[23, ], date_time:d2H_p_t)
-sd(LRD_tidy$d18O[LRD_tidy$source == "River"])
-sd(LRD_tidy$d2H[LRD_tidy$source == "River"])
-sd(LRD_tidy$d18O[LRD_tidy$source == "Rain"])
-sd(LRD_tidy$d2H[LRD_tidy$source == "Rain"])
-# Use this in Table 1.
-
-# Visualise results ------------------------------------------------------------
-
-# Heavy data wrangling
-LRD_new_for_plot <- LRD_new %>%
-    select(date_time:d2H_p_t) %>%
-    gather(key = isotope_dE_stream_t, value = dE_stream_t,
-           d18O_stream_t:d2H_stream_t) %>%
-    mutate(dE_stream_isotope = as_vector(map(str_split(isotope_dE_stream_t, "_"),
-                                             `[[`,
-                                             1))) %>%
-    select(-isotope_dE_stream_t) %>%
-    gather(key = isotope_dE_rain_t, value = dE_rain_t,
-           d18O_rain_t:d2H_rain_t) %>%
-    mutate(
-        dE_rain_isotope = as_vector(map(str_split(isotope_dE_rain_t, "_"),
-                                        `[[`,
-                                        1))) %>%
-    select(-isotope_dE_rain_t) %>%
-    gather(key = source, value = dE_t,
-           dE_stream_t, dE_rain_t) %>%
-    mutate(source = ifelse(str_detect(source, "stream"),
-                           "stream",
-                           "rain")) %>%
-    mutate(isotope = ifelse(source == "stream",
-                            dE_stream_isotope,
-                            dE_rain_isotope)) %>%
-    select(-dE_stream_isotope, -dE_rain_isotope) %>%
-    gather(key = p_isotope, value = p_t,
-           d18O_p_t, d2H_p_t) %>%
-    mutate(p_isotope = as_vector(map(str_split(p_isotope, "_"),
-                                     `[[`,
-                                     1))) %>%
-    filter(p_isotope == isotope) %>%
-    select(-p_isotope)
-
-# Fig. 4. Cumulative amount-weighted isotope values' averages through the storm
-my_palette <- c(
-    "#cd6c64",  # light red
-    "#8dcf6d",  # light green
-    "#983b33",  # dark red
-    "#44782b"   # dark green
+    mutate(d18O_x_amnt = d18O * amnt,
+           d2H_x_amnt = d2H * amnt) %>%
+    summarise(weighted_d18O = sum(d18O_x_amnt) / sum(amnt),
+              weighted_d2H = sum(d2H_x_amnt) / sum(amnt)) %$%
+    list(d18O = weighted_d18O,
+         d2H = weighted_d2H)
+# Uncertainty (u)
+u <- list(
+    d18O = NULL,  # TODO
+    d2H = NULL
 )
-fig_4 <- ggplot(LRD_new_for_plot,
-                aes(date_time, dE_t,
-                    col = paste(source, isotope))) +
-    geom_point(aes(shape = source)) +
-    geom_line(aes(group = source)) +
-    ylab(expression(paste(delta^{2}, "H (‰)",
-                          "                                        ",
-                          delta^{18}, "O (‰)"))) +
-    scale_color_manual(values = my_palette) +
-    scale_shape_manual(values = c(17, 19), labels = c("Rain", "River")) +
-    scale_x_datetime(name = "Date & time", date_labels = "%Y-%m-%d %H:%M") +
-    facet_grid(isotope ~ ., scales = "free_y") +
-    theme_bw() +
-    theme(legend.position    = "left",
-          legend.title       = element_blank(),
-          legend.background  = element_rect(fill = NA, colour = NA),
-          strip.background   = element_rect(fill = NA, colour = NA),
-          strip.text.y       = element_blank(),
-          panel.grid         = element_blank(),
-          panel.grid.major.x = element_line(colour = "grey50", linetype = "dashed"),
-          panel.grid.minor.x = element_line(colour = "grey75", linetype = "dashed"),
-          axis.text.x        = element_text(angle = 0))
 
-# Fig. 5. Proportion of streamflow made up by rain water,
-# according to both d2H and d18O, base on cumulative isotope values' averages
-# through the storm
-my_palette2 <- c(
-    "#b73f41",  # red
-    "#599f37"   # green
+# Calculations -----------------------------------------------------------------
+
+# Uncertainty (u_q) propagation rules
+#   1. Constants
+#       q = bx
+#       u_q / q = u_x / x
+#   2. Sums
+#       q = x + y + z
+#       u_q = sqrt(u_x^2 + u_y^2 + u_z^2)
+#   3. Products/quotients
+#       q = xyz
+#       u_q / abs(q) = sqrt((u_x / x)^2 + (u_y / y)^2 + (u_z / z)^2)
+
+p <- list(d18O = NULL, d2H = NULL)
+u_p <- list(d18O = NULL, d2H = NULL)
+
+p$d18O <- (streamflow$d18O - baseflow$d18O) / (rain$d18O - baseflow$d18O)
+u_p$d18O <- abs(p$d18O) * sqrt(
+    (sqrt(u$d18O^2 + u$d18O^2) / (streamflow$d18O - baseflow$d18O)) ^ 2 +
+    (sqrt(u$d18O^2 + u$d18O^2) / (rain$d18O - baseflow$d18O)) ^ 2
 )
-fig_5 <- ggplot(LRD_new_for_plot,
-                aes(date_time, p_t * 100,
-                    col = isotope)) +
-    geom_point() +
-    geom_line(aes(group = isotope)) +
-    ylab("Percent rainfall (%)") +
-    scale_color_manual(
-        values = my_palette2,
-        labels = c(expression(paste(delta^{18}, "O (‰)")),
-                   expression(paste(delta^{2}, "H (‰)")))) +
-    scale_x_datetime(name = "Date & time", date_labels = "%Y-%m-%d %H:%M") +
-    theme_bw() +
-    theme(legend.position    = c(0.9, 0.1),
-          legend.title       = element_blank(),
-          legend.background  = element_rect(fill = NA, colour = NA),
-          strip.background   = element_rect(fill = NA, colour = NA),
-          strip.text.y       = element_blank(),
-          panel.grid         = element_blank(),
-          panel.grid.major.x = element_line(colour = "grey50", linetype = "dashed"),
-          panel.grid.minor.x = element_line(colour = "grey75", linetype = "dashed"),
-          axis.text.x        = element_text(angle = 0))
 
-tiff(here::here("figures/fig-4.tiff"), width = 15, height = 10, units = "cm", res = 500)
-fig_4
-dev.off()
+p$d2H <- (streamflow$d2H - baseflow$d2H) / (rain$d2H - baseflow$d2H)
+u_p$d2H <- abs(p$d2H) * sqrt(
+    (sqrt(u$d2H^2 + u$d2H^2) / (streamflow$d2H - baseflow$d2H)) ^ 2 +
+    (sqrt(u$d2H^2 + u$d2H^2) / (rain$d2H - baseflow$d2H)) ^ 2
+)
 
-tiff(here::here("figures/fig-5.tiff"), width = 15, height = 10, units = "cm", res = 500)
-fig_5
-dev.off()
+p_rain <- (p$d18O + p$d2H) / 2
+u_p_rain <-  abs(p_rain) * (sqrt(u_p$d18O^2 + u_p$d2H^2) / (p$d18O + p$d2H))
+
+# Done
+p_rain
+u_p_rain
