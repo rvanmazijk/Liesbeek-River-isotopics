f5c0bc4694da5471082fe32981d12fbe7175b10d
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-06-27 17:15:16 +0200

    Cont mass-balance

--- analyses/04_mass-balance.R
+++ analyses/04_mass-balance.R
@@ -3,25 +3,17 @@
 
 source(here::here("setup.R"))
 
-# Define baseflow isotope values -----------------------------------------------
+# Define baseflow, streamflow and rain isotope values --------------------------
 
-baseflow <- list(d18O = NULL, d2H = NULL)
-
-baseflow$d18O <- LRD_tidy %>%
+baseflow <- LRD_tidy %>%
     filter(source == "River") %>%
-    slice(1:3) %$% {
-        sum(d18O[1:3] * amnt[1:3]) %>%
-            divide_by(sum(amnt[1:3]))
-    }
-baseflow$d2H <- LRD_tidy %>%
-    filter(source == "River") %>%
-    slice(1:3) %$% {
-        sum(d2H[1:3] * amnt[1:3]) %>%
-            divide_by(sum(amnt[1:3]))
-    }
-
-# Separate rain and stream data ------------------------------------------------
-
+    slice(1:3) %>%
+    mutate(d18O_x_amnt = d18O * amnt,
+           d2H_x_amnt = d2H * amnt) %>%
+    summarise(weighted_d18O = sum(d18O_x_amnt) / sum(amnt),
+              weighted_d2H = sum(d2H_x_amnt) / sum(amnt)) %$%
+    list(d18O = weighted_d18O,
+         d2H = weighted_d2H)
 streamflow <- LRD_tidy %>%
     filter(source == "River") %>%
     slice(-c(1:3)) %>%
@@ -39,10 +31,19 @@ rain <- LRD_tidy %>%
               weighted_d2H = sum(d2H_x_amnt) / sum(amnt)) %$%
     list(d18O = weighted_d18O,
          d2H = weighted_d2H)
+
 # Uncertainty (u)
+precision <- list(
+    d18O = 0.07,
+    d2H = 0.2
+)
+accuracy <- list(
+    d18O = 0.13,
+    d2H = 1.5
+)
 u <- list(
-    d18O = NULL,  # TODO
-    d2H = NULL
+    d18O = sqrt(precision$d18O ^ 2 + accuracy$d18O ^ 2),
+    d2H = sqrt(precision$d2H ^ 2 + accuracy$d2H ^ 2)
 )
 
 # Calculations -----------------------------------------------------------------
@@ -57,6 +58,8 @@ u <- list(
 #   3. Products/quotients
 #       q = xyz
 #       u_q / abs(q) = sqrt((u_x / x)^2 + (u_y / y)^2 + (u_z / z)^2)
+
+# And using "Ruan's propagation" from uncertainty-propagation.html
 
 p <- list(d18O = NULL, d2H = NULL)
 u_p <- list(d18O = NULL, d2H = NULL)
@@ -74,8 +77,42 @@ u_p$d2H <- abs(p$d2H) * sqrt(
 )
 
 p_rain <- (p$d18O + p$d2H) / 2
-u_p_rain <-  abs(p_rain) * (sqrt(u_p$d18O^2 + u_p$d2H^2) / (p$d18O + p$d2H))
+u_p_rain <- abs(p_rain) * (sqrt(u_p$d18O^2 + u_p$d2H^2) / (p$d18O + p$d2H))
 
 # Done
 p_rain
 u_p_rain
+
+# Calculations--Genereux 1998 method
+
+u_p_E <- function(baseflow, rain, streamflow, u) {
+    return(sqrt(
+        (u * ((rain - streamflow) / (rain - baseflow)^2))^2 +
+        (u * ((streamflow - baseflow) / (rain - baseflow)^2))^2 +
+        (u * (-1 / (rain - baseflow))^2)
+    ))
+}
+u_p_E_Ruan <- function(baseflow, rain, u) {
+    return(u * (2 / (rain - baseflow)))
+}
+u_p_d18O_Genereux <- u_p_E(
+    baseflow$d18O,
+    rain$d18O,
+    streamflow$d18O,
+    u$d18O
+)
+u_p_d2H_Genereux <- u_p_E(
+    baseflow$d2H,
+    rain$d2H,
+    streamflow$d2H,
+    u$d2H
+)
+p_rain_Genereux <- (p$d18O + p$d2H) / 2
+u_p_rain_Genereux <-  abs(p_rain_Genereux) * (sqrt(u_p_d18O_Genereux^2 + u_p_d2H_Genereux^2) / (p$d18O + p$d2H))
+
+
+# Done
+u_p_d18O_Genereux
+u_p_d2H_Genereux
+p_rain_Genereux
+u_p_rain_Genereux
